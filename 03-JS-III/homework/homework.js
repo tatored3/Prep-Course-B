802bc17d29a97af1d6701f58cdcc82bb023c7e8e
tatored3 <tatored3@gmail.com>
2022-09-16 02:46:30 +0000

    Tarea 03-JS-III

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,51 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    let suma =0;
+    for (let i = 1; i <= 10; i++){
+      suma += i;
+    }
+    return suma;
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  resultado = [];
+  for (let i = 0; i <= array.length-1; i++){
+    if (array[i] % 2 === 0)
+      resultado.push(array[i]);
+  }
+  return resultado;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  resultado = [];
+  for (let i = 0; i <= array.length-1; i++){
+    resultado.push(Math.pow(array[i],2));
+  }
+  return resultado;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  resultado = 0;
+  for (let i = 0; i <= array.length-1; i++){
+    resultado = resultado + array[i];
+  }
+  return resultado;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return num.toString().length;
 }
   
   
